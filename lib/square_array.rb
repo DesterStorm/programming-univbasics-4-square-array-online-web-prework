104f80c04a9a0d33f1c787cc5409e539fb98ff57
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-13 22:43:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,21 @@
 
 def square_array(numbers) 
   	counter = 0 
+<<<<<<< HEAD
  	    while counter < numbers.size do
       numbers[counter] = numbers[counter] * numbers[counter]
       counter += 1
       end
 	  numbers  
+=======
+  
+ 	while counter < numbers.size do 
+      numbers[counter] = numbers[counter] * numbers[counter]
+      counter++
+      
+    end 
+  
+  	numbers
+  
+>>>>>>> 44fd8b53f25b0286bb543ed8817dace36641f353
 end
